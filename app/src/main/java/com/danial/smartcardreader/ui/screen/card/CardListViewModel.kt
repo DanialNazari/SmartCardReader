8dffd7fc151922dfb6ccd1430ad7c3de9a07f2ca
Danial Nazari <DanialNazari43@gmail.com>
2024-03-17 10:35:07 +0330

    Optimize imports

--- app/src/main/java/com/danial/smartcardreader/ui/screen/card/CardListViewModel.kt
+++ app/src/main/java/com/danial/smartcardreader/ui/screen/card/CardListViewModel.kt
@@ -1,8 +1,5 @@
 package com.danial.smartcardreader.ui.screen.card
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.danial.smartcardreader.model.CardItemModel
